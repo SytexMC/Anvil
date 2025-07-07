50dbfe9db7ad7a4b4dbee94345d81c1c2f0dd96e
Sytex <sytex@duck.com>
2025-07-07 19:59:31 +0200

    build: downgrade project from Java 21 to Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ allprojects {
     group = "me.sytex"
     version = "0.0.1-SNAPSHOT"
 
-    ext { set("javaToolchainVersion", 21) }
+    ext { set("javaToolchainVersion", 17) }
 
     tasks.withType<Jar> {
         archiveBaseName.set(rootProject.name)
